836ff20b45d63929c942d657a959736594b7abdc
lithiumdenis <lithiumdenis@gmail.com>
2015-12-20 13:31:54 +0300

    step back, wrong way

--- filesystem.c
+++ filesystem.c
@@ -1,14 +1,8 @@
 #define FUSE_USE_VERSION 26
-#define _FILE_OFFSET_BITS 64
-#define NLENGTH 512
-#define FREEBLOCK 0
-#define FOLDERBLOCK 1
-#define FILEBLOCK 2
 
+#define DEBUG 
 #ifdef DEBUG
-#define TRACE printf("[DEBUG] FILE:%s LINE:%d\n", __FILE__, __LINE__);
-#else
-#define TRACE
+	#define TRACE printf("%d\n", __LINE__);
 #endif
 
 #include <fuse.h>
@@ -16,80 +10,184 @@
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <limits.h>
-#include <unistd.h>
-#include <stdlib.h>
 
 static const char *binary_path = "/home/lithiumdenis/CSF/Filesystem/MyBinaryFile/bigbinary";
-const int blockSize = 4096;
-int fileDest = -1;
-const int isRootBlock = 0;
 
-//Данные о файле
-typedef struct stat stat_t;
-typedef struct node inode_t;
-struct node
+typedef struct str_iFolder 
+{ 
+	char name[10][5];
+	int nodes[10];
+}iFolder;
+
+typedef struct str_iNode *inode;
+struct str_iNode
 {
-    char name[NLENGTH];
-    char data[0];
-    char status;
-    stat_t stat;
+	int type;
+	iFolder folder;
 };
 
-//Создаём блок
-void *blockCreator()
+struct str_FileSystem 
 {
-    return calloc(blockSize, sizeof(char));
+	int istart;
+	int iend;
+	int isize;
+}fileSystem;
+
+int KnowSizeFile()
+{
+	FILE* fs = fopen(binary_path, "rb+");
+	fseek(fs, 0, SEEK_END);   
+	int size = ftell(fs);
+	return size; 	
 }
 
-//Записываем блок
-int blockWriter(int number, void *block)
+void FillBinaryFile() 
 {
-    int result = -1;
-    if (number >= 0 && lseek(fileDest, blockSize * number, SEEK_SET) >= 0)
+        //Заполняем файл нулями
+        FILE* f = fopen(binary_path, "wt");
+	for (int i = 0; i < 10000000; i++)
+	{
+		fprintf(f, "%d\n", 0);
+		fprintf(f, "\n");
+	}
+	fclose(f);
+        
+        //Форматируем для работы с iNode
+	int size = KnowSizeFile();
+	FILE* fs = fopen(binary_path, "rb+"); 
+	fseek(fs, 0, SEEK_SET);
+	fileSystem.isize = sizeof(struct str_iNode);
+	fileSystem.istart = sizeof(struct str_FileSystem);
+	fileSystem.iend = size;
+	fwrite(&fileSystem, sizeof(struct str_FileSystem), 1, fs);
+	for (int i = fileSystem.istart; i < fileSystem.iend - fileSystem.isize; i += fileSystem.isize) 
+	{
+		fseek(fs, i, SEEK_SET);  
+		int x = 0;
+		fwrite(&x, 4, 1, fs);
+	}
+	int type = 1;
+	inode in = malloc(sizeof(struct str_iNode));
+	in->type = type;
+	for (int i = 0; i < 10; i++) 
+	{
+		in->folder.nodes[i] = NULL;
+	}
+	fseek(fs, fileSystem.istart, SEEK_SET);
+	fwrite(in, sizeof(struct str_iNode), 1, fs);
+
+	fclose(fs);
+}
+
+inode ReadiNode(int index) 
+{
+    FILE* fs = fopen(binary_path, "rb+");
+    fseek(fs, index, SEEK_SET);
+    inode in = malloc(sizeof(struct str_iNode));
+	fread(in, sizeof(struct str_iNode), 1, fs);
+    fclose(fs);
+    return in;
+}
+
+void PrintiNode(inode n) 
+{
+    if (n->type == 1) 
     {
-        if (write(fileDest, block, blockSize) == blockSize)
+        for (int i = 0; i < 10; i++) 
         {
-            result = 0;
+	        printf("%d, %d, %s, %d\n", i, n->folder.nodes[i], n->folder.name[i], n->type);
         }
     }
-    return result;
 }
 
-//Создаем корневой элемент
-int rootCreator()
+int AddChildiNode(inode parent, int parentIndex, inode child, const char* nameChild)
 {
-    int res = -1;
-    inode_t *rootNode = (inode_t *)blockCreator();
-    if (rootNode != NULL)
-    {
-        rootNode->name[0] = '\0';
-        rootNode->status = FOLDERBLOCK;
-        rootNode->stat.st_mode = S_IFDIR | 0777;
-        rootNode->stat.st_nlink = 2;
-        if (blockWriter(isRootBlock, rootNode) == 0)
-        {
-            res = 0;
-        }
-        free(rootNode);
-    }
-    return res;
+	int pos = FindFreeiNode();
+	FILE *fs = fopen(binary_path, "rb+");	
+	if (pos < 0 || parent->type != 1)
+	{
+		return -1;
+	}
+	int i = 0;
+	while (i < 10 && parent->folder.nodes[i] != NULL)
+	{
+		i++;
+	}
+	if (i == 10)
+	{
+		return -1;
+	}
+	parent->folder.nodes[i] = pos;
+	CopyName(parent->folder.name[i], nameChild);	
+	fseek(fs, parentIndex, SEEK_SET);
+	fwrite(parent, sizeof(struct str_iNode), 1, fs);
+	fseek(fs, pos, SEEK_SET);
+	fwrite(child, sizeof(struct str_iNode), 1, fs);
+	fclose(fs);
+	return pos;
 }
 
-//Создаём файл или пытаемся работать с известным
-void makeBinaryFile()
+void CopyName(char* dest, char* source) 
 {
-    //Попытаемся открыть файл
-    fileDest = open(binary_path, O_RDWR, 0666);
-    if (fileDest < 0)
+    int len = strlen(source);
+    int i = 0;
+    for (i; i < len; i++)
     {
-        //Если не нашли, создадим новый
-        fileDest = open(binary_path, O_CREAT | O_RDWR, 0666);
-        rootCreator();
+        dest[i] = source[i];
+    }
+    dest[i] = NULL;
+}
+
+inode FindINode(char* path)
+{   
+    if (strcmp(path, "/") == 0)
+    {
+        return ReadiNode(fileSystem.istart);
+    }
+    else
+    {
+	if (strcmp(path, "/qq") == 0)
+    	{
+    		inode n = ReadiNode(fileSystem.istart + fileSystem.isize);
+    		if (n->type == 0) 
+    			return NULL;
+       		return ReadiNode(fileSystem.istart + fileSystem.isize);
+    	}
+        return NULL;
     }
 }
 
-/*//Получение атрибутов файла
+int FindFreeiNode()
+{
+	FILE *fs = fopen(binary_path, "rb+");
+	inode n = malloc(sizeof(struct str_iNode));	
+	int pos = fileSystem.istart;
+	do
+	{
+		if (pos >= fileSystem.iend - fileSystem.isize)
+		{
+			return -1;
+		}
+		fseek(fs, pos, SEEK_SET);
+		fread(n, sizeof(struct str_iNode), 1, fs);
+		if (n->type == 0)
+		{
+			return pos;
+		}
+		pos += fileSystem.isize;
+	}while (pos < fileSystem.iend);
+	return pos;
+}
+
+void LoadFileSystem() 
+{
+    FILE* fs = fopen(binary_path, "rb+");
+    fseek(fs, 0, SEEK_SET);
+    fread(&fileSystem, sizeof(struct str_FileSystem), 1, fs);
+    fclose(fs);
+}
+
+//Получение атрибутов файла
 static int lithiumdenis_getattr(const char *path, struct stat *stbuf)
 {
     inode n = FindINode(path);
@@ -144,7 +242,7 @@ static int lithiumdenis_mkdir(const char* path, mode_t mode)
 	AddChildiNode(parent, fileSystem.istart, child, name);
 	PrintiNode(parent);
 	return 0;
-}*/
+}
 
 //Изменение размера файла
 static int lithiumdenis_truncate(const char * path, off_t offset) {
@@ -152,15 +250,16 @@ static int lithiumdenis_truncate(const char * path, off_t offset) {
 }
 
 static struct fuse_operations lithiumdenis_operations = {
-	//.getattr  = lithiumdenis_getattr,
-	//.readdir  = lithiumdenis_readdir,
-	//.mkdir    = lithiumdenis_mkdir,
+	.getattr  = lithiumdenis_getattr,
+	.readdir  = lithiumdenis_readdir,
+	.mkdir    = lithiumdenis_mkdir,
         .truncate = lithiumdenis_truncate
 };
 
 
 int main(int argc, char *argv[])
 {
-        makeBinaryFile();
+        FillBinaryFile();
+        LoadFileSystem();
 	return fuse_main(argc, argv, &lithiumdenis_operations, NULL);
 }
